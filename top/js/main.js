d5273c93f3cbc80ed39527a06b76713d16577502
webarata3 <webarata3@gmail.com>
2018-10-23 18:42:40 +0900

    全体の見直し

--- top/js/main.js
+++ top/js/main.js
@@ -16,3 +16,27 @@ modalEl.addEventListener('click', (event) => {
   modalEl.classList.remove('show');
   modalEl.classList.add('hide');
 });
+
+const userAgent = window.navigator.userAgent.toLowerCase();
+
+const warningMs = `
+<div id="warning">!!! お使いのブラウザでは、このサイトの情報が正しく表示されません。</div>
+ <div id="ms">
+  <img src="image/chrome.png">
+  <div class="chrome">
+  <div>Google Chromeを今すぐお試しください</div>
+ <div>Windows 10のための高速で安全なブラウザー</div>
+ </div>
+ <div class="confirm_chrome">
+  <span>いいえ結構です</span>
+  <a href="https://www.google.com/intl/ja_ALL/chrome/">今すぐ使う</a>
+ </div>
+</div>`;
+
+if (userAgent.indexOf('edge') !== -1) {
+  const ms = document.createElement('div');
+  ms.innerHTML = warningMs;
+
+  const body = document.querySelector('body');
+  body.insertBefore(ms, body.firstChild);
+}
