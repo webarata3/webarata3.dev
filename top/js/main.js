9ac052d5180232f7b88c6a3e8678adbd1489759e
webarata3 <webarata3@gmail.com>
2018-10-24 18:28:38 +0900

    MSの警告をアニメーションさせる

--- top/js/main.js
+++ top/js/main.js
@@ -21,11 +21,11 @@ const userAgent = window.navigator.userAgent.toLowerCase();
 
 const warningMs = `
 <div id="warning">!!! お使いのブラウザでは、このサイトの情報が正しく表示されません。</div>
- <div id="ms">
-  <img src="/image/chrome.png">
-  <div class="chrome">
+<div id="ms">
+ <img src="image/chrome.png">
+ <div class="chrome">
   <div>Google Chromeを今すぐお試しください</div>
- <div>Windows 10のための高速で安全なブラウザー</div>
+  <div>Windows 10のための高速で安全なブラウザー</div>
  </div>
  <div class="confirm_chrome">
   <span>いいえ結構です</span>
@@ -35,8 +35,12 @@ const warningMs = `
 
 if (userAgent.indexOf('edge') !== -1) {
   const ms = document.createElement('div');
+  ms.setAttribute('id', 'browserWarning');
   ms.innerHTML = warningMs;
 
   const body = document.querySelector('body');
   body.insertBefore(ms, body.firstChild);
+  setTimeout(() => {
+    ms.classList.add('show');
+  }, 1200);
 }
