1b6b119dae4f8fb2a00a4a68a3c155d8cf204f41
webarata3 <webarata3@gmail.com>
2018-11-01 19:09:19 +0900

    WORK

--- top/js/work.js
+++ top/js/work.js
@@ -1,36 +1,44 @@
 'use strict';
 
-const works = document.querySelectorAll('.works > article');
-let position = 0;
+const works = document.querySelectorAll('.works > div > article');
 const worksCount = works.length;
 
 function resetPosition(position) {
   works.forEach((work, index) => {
     const loc = index + position;
-    work.classList.remove('work');
     work.style.left = 180 + loc * 500 + 'px';
     let scale = loc >= 0 ? 1 - 0.5 * loc : 1 + 0.5 * loc;
     scale = scale < 0 ? 0 : scale;
     work.style.transform = `scale(${scale}, ${scale})`;
+    console.log(scale);
+    if (scale === 1.0) {
+      work.style.boxShadow = '2px 2px 4px';
+    } else {
+      work.style.boxShadow = 'none';
+    }
   });
 }
 
+let position = 0;
 resetPosition(position);
 
-document.querySelector('main').addEventListener('click', event => {
-  if (event.button === 0 && !event.shiftKey) {
-    if (position === -(worksCount - 1)) return;
-    works.forEach((elm, index) => {
-      elm.style.animation = `left${index + position + 1} 0.6s ease 0s 1 normal forwards`;
-    });
-    position--;
-    resetPosition(position);
-  } else if (event.shiftKey) {
-    if (position === 0) return;
-    works.forEach((elm, index) => {
-      elm.style.animation = `right${index + position + 2} 0.6s ease 0s 1 normal forwards`;
-    });
-    position++;
-    resetPosition(position);
-  }
+const moveLeftEl = document.querySelector('.moveLeft');
+const moveRightEl = document.querySelector('.moveRight');
+
+moveLeftEl.addEventListener('click', () => {
+  if (position === -(worksCount - 1)) return;
+  works.forEach((elm, index) => {
+    elm.style.animation = `left${index + position + 1} 0.6s ease 0s 1 normal forwards`;
+  });
+  position--;
+  resetPosition(position);
+});
+
+moveRightEl.addEventListener('click', () => {
+  if (position === 0) return;
+  works.forEach((elm, index) => {
+    elm.style.animation = `right${index + position + 2} 0.6s ease 0s 1 normal forwards`;
+  });
+  position++;
+  resetPosition(position);
 });
