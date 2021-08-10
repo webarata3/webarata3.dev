4b16ec0cba36de3246666a5b29426cf0338942da
webarata3 <webarata3@gmail.com>
2021-08-10 16:59:11 +0900

    回転のテスト

--- public/js/skill.js
+++ public/js/skill.js
@@ -2,110 +2,79 @@
   const init = (tabButtonsSelector, tabContentSelector) => {
     const tabButtons = document.querySelectorAll(tabButtonsSelector);
     const tabContent = document.querySelector(tabContentSelector);
-
-    placementTab(tabButtons, tabContent);
-    initTab(tabButtons);
+    const rotateTab = new RotateTab(tabButtons, tabContent);
   };
 
   const getPx = (elm, property) => {
     return parseInt(getComputedStyle(elm).getPropertyValue(property).replace('px', ''));
   };
 
-  const placementTab = (tabButtons, tabContent) => {
-    const r = getPx(tabContent, 'width') / 2;
-    const top = getPx(tabContent, 'top');
-    const left = getPx(tabContent, 'left');
-    const MARGIN = 5;
+  class RotateTab {
+    constructor(tabButtons, tabContent) {
+      this.tabButtons = tabButtons;
+      this.tabContent = tabContent;
+      this.h = getPx(this.tabButtons[0], 'height');
+      this.r = getPx(this.tabContent, 'width') / 2;
+      this.top = getPx(this.tabContent, 'top');
+      this.left = getPx(this.tabContent, 'left');
+      this.betweenAngle = 360 / this.tabButtons.length;
+      this.#rotate(0, false);
+      this.#setEvent();
+    }
 
-    const count = tabButtons.length;
-    const betweenDeg = 2 / count * Math.PI;
-    const baseX = 0;
-    const baseY = r;
+    #rotate(changeDeg, animation = true) {
+      for (let i = 0; i < this.tabButtons.length; i++) {
+        this.tabButtons[i].style.position = 'absolute';
+        const w = getPx(this.tabButtons[i], 'width');
+        this.tabButtons[i].style.top = `${this.top - this.h / 2}px`;
+        this.tabButtons[i].style.left = `${this.left + this.r - w / 2}px`;
 
-    const tabContents = Array.from(tabContent.childNodes).filter(node => node.nodeType === Node.ELEMENT_NODE);
-    for (let i = 0; i < count; i++) {
-      let theta = i * betweenDeg;
-      let x = Math.round(baseX * Math.cos(theta) - baseY * Math.sin(theta));
-      let y = Math.round(baseX * Math.sin(theta) + baseY * Math.cos(theta));
-      tabButtons[i].style.position = 'absolute';
-      const w = getPx(tabButtons[i], 'width');
-      const h = getPx(tabButtons[i], 'height');
-      if (x < 0) x = x - w;
-      if (y > 0) y = y + h;
-      // 横が中心の場合上下に少し間隔を空ける
-      if (x === 0) {
-        x = x - (w / 2);
-        y = y + (y <= 0 ? -MARGIN : MARGIN);
+        const newAngle = (this.betweenAngle * i + changeDeg + 360) % 360;
+        console.log(i, newAngle);
+        this.tabButtons[i].style.transform = `rotate(${newAngle}deg)`;
+        this.tabButtons[i].style.transformOrigin = `0 ${this.r}px`;
+        this.tabButtons[i].dataset.deg = 360 - newAngle;
+        if (animation) this.tabButtons[i].style.transition = 'transform 1s ease-out';
+
+        const inner = this.tabButtons[i].querySelector('p');
+        inner.style.transform = `rotate(-${newAngle}deg)`;
+        inner.style.transformOrigin = '0 0';
+        if (animation) inner.style.transition = 'transform 1s ease-out';
       }
-      tabButtons[i].style.top = `${-y + r + top}px`;
-      tabButtons[i].style.left = `${x + r + left}px`;
-      const charaRgb = hsvToRgb(Math.round(theta * 57.2958), 1.0, 0.8);
-      const circleRgb = hsvToRgb(Math.round(theta * 57.2958), 0.7, 1.0);
-      const tabButtonColor = `rgb(${charaRgb.r}, ${charaRgb.g}, ${charaRgb.b})`;
-      tabButtons[i].style.color = tabButtonColor;
-      tabButtons[i].style.borderColor = tabButtonColor;
-      tabContents[i].dataset.circleColor = `rgb(${circleRgb.r}, ${circleRgb.g}, ${circleRgb.b})`;
-    }
-  };
-
-  const hsvToRgb = (h, s, v) => {
-    const max = v;
-    const min = max - ((s / 255) * max);
-    const rgb = { 'r': 0, 'g': 0, 'b': 0 };
-
-    h = h % 360;
-
-    if (s === 0) {
-      rgb.r = v * 255;
-      rgb.g = v * 255;
-      rgb.b = v * 255;
-      return rgb;
     }
 
-    const dh = Math.floor(h / 60);
-    const p = v * (1 - s);
-    const q = v * (1 - s * (h / 60 - dh));
-    const t = v * (1 - s * (1 - (h / 60 - dh)));
-
-    switch (dh) {
-      case 0: rgb.r = v; rgb.g = t; rgb.b = p; break;
-      case 1: rgb.r = q; rgb.g = v; rgb.b = p; break;
-      case 2: rgb.r = p; rgb.g = v; rgb.b = t; break;
-      case 3: rgb.r = p; rgb.g = q; rgb.b = v; break;
-      case 4: rgb.r = t; rgb.g = p; rgb.b = v; break;
-      case 5: rgb.r = v; rgb.g = p; rgb.b = q; break;
-    }
-
-    rgb.r = Math.round(rgb.r * 255);
-    rgb.g = Math.round(rgb.g * 255);
-    rgb.b = Math.round(rgb.b * 255);
-    return rgb;
-  };
-
-  const initTab = tabButtons => {
-    let currentTab = null;
-    let currentContent = null;
-    tabButtons.forEach(tabButton => {
-      tabButton.addEventListener('click', () => {
-        if (currentTab !== null) {
-          currentContent.classList.add('skill__tab-content-init');
-        }
-        const content = document.querySelector(`#${tabButton.dataset.content}`);
-        content.classList.remove('skill__tab-content-init');
-        content.parentNode.style.borderColor = content.dataset.circleColor;
-        currentTab = tabButton;
-        currentContent = content;
+    #setEvent() {
+      this.first = true;
+      this.currentTab = null;
+      this.currentContent = null;
+      this.tabButtons.forEach(tabButton => {
+        tabButton.addEventListener('click', () => {
+          if (this.currentTab !== null) {
+            this.currentContent.classList.add('skill__tab-content-init');
+          }
+          const content = document.querySelector(`#${tabButton.dataset.content}`);
+          content.classList.remove('skill__tab-content-init');
+          content.parentNode.style.borderColor = content.dataset.circleColor;
+          this.currentTab = tabButton;
+          this.currentContent = content;
+          if (this.first) {
+            this.first = false;
+          } else {
+            this.#rotate(tabButton.dataset.deg);
+            console.log(tabButton.dataset.deg);
+          }
+        });
       });
-    });
 
-    clickFirstTabButton(tabButtons[0]);
-  };
+      this.#clickFirstTabButton(this.tabButtons[0]);
+    }
 
-  const clickFirstTabButton = tabButton => {
-    const event = document.createEvent('MouseEvents');
-    event.initEvent('click', false, true);
-    tabButton.dispatchEvent(event);
-  };
+    #clickFirstTabButton(tabButton) {
+      const event = document.createEvent('MouseEvents');
+      event.initEvent('click', false, true);
+      tabButton.dispatchEvent(event);
+    }
+  }
 
   init('#skillTabButtons > li', '.skill__tab-content');
 })(window);
